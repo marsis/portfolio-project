7cc6265ab6be517e12b7512bd8b4856e747ac990
A.Logozinskaya <logozynski@gmail.com>
2020-08-07 17:07:47 +0300

    add cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,6 +13,16 @@ app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
 app.use(background);
+
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 let server = app.listen(process.env.PORT || 3000, function () {
     let port = server.address().port;
     console.log("App now running on port", port);
